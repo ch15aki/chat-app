6e9ebe005a20fee26323fa3dcdf72fd4ee763160
ch15aki <and.1la.view@gmail.com>
2020-07-25 23:27:56 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ApplicationRecord
     # アソシエーション
     has_many :room_users
-    has_many :users, through: :room_users
-    has_many :messages
+    has_many :users, through: :room_users, dependent: :destroy
+    has_many :messages, dependent: :destroy
     # バリデーション
     validates :name, presence: true
 end
