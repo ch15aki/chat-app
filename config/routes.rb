6621a6ebbcf02df58440dfa927ab2f86920c455e
ch15aki <and.1la.view@gmail.com>
2020-07-25 18:10:25 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root 'rooms#index'
   #ユーザー編集のビューファイルを表示する
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
